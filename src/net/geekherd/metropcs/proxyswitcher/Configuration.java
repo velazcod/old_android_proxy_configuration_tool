877a8b617f5cfabaf04a2c8ddf2de6f195792561
Daniel Velazco <velazcod@gmail.com>
2010-08-10 17:52:16 -0400

    setting up environment

---
+++ src/net/geekherd/metropcs/proxyswitcher/Configuration.java
@@ -0,0 +1,13 @@
+package net.geekherd.metropcs.proxyswitcher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Configuration extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
