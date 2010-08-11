a299c9838391ee445a2de575ad2ff2156b28b041
Daniel Velazco <velazcod@gmail.com>
2010-08-10 21:38:30 -0400

    Added preferences constants and began coding to support u2nl

--- src/net/geekherd/metropcs/proxyswitcher/Configuration.java
+++ src/net/geekherd/metropcs/proxyswitcher/Configuration.java
@@ -1,6 +1,7 @@
 package net.geekherd.metropcs.proxyswitcher;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class Configuration extends Activity 
@@ -10,11 +11,34 @@ public class Configuration extends Activity
 	public final static String DEFAULT_PROXY_PORT = "3128"; // MetroPCS Proxy Server Port
 	
 	
+	/** Preferences Constants **/
+	public final static String PREF_AUTO_SWITCH_ENABLED = "prefs_autoswitch_enabled";
+	public final static Boolean PREF_AUTO_SWITCH_ENABLED_DEFAULT = true;
+	
+	public final static String PREF_USE_U2NL = "prefs_use_u2nl";
+	public final static Boolean PREF_USE_U2NL_DEFAULT = true;
+	
+	public final static String PREF_USE_CUSTOM_PROXY = "prefs_use_custom_proxy";
+	public final static Boolean PREF_USE_CUSTOM_PROXY_DEFAULT = false;
+	
+	public final static String PREF_PROXY = "prefs_custom_proxy";
+	public final static String PREF_PROXY_DEFAULT = DEFAULT_PROXY;
+	
+	public final static String PREF_PROXY_PORT = "prefs_custom_proxy_port";
+	public final static String PREF_PROXY_PORT_DEFAULT = DEFAULT_PROXY_PORT;
+	/** Preferences Constants **/
+	
+	
+	private SharedPreferences preferences;
+	
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
         
         /*
          * TODO:
--- src/net/geekherd/metropcs/proxyswitcher/WifiReceiver.java
+++ src/net/geekherd/metropcs/proxyswitcher/WifiReceiver.java
@@ -4,21 +4,32 @@ import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.NetworkInfo;
 import android.net.Proxy;
 import android.net.wifi.WifiManager;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.util.Log;
 
 public class WifiReceiver extends BroadcastReceiver 
 {
 	private Context context;
+	private SharedPreferences preferences;
 	
 	@Override
 	public void onReceive(Context context, Intent intent) 
 	{
-		final String action = intent.getAction();
 		this.context = context;
+		preferences = PreferenceManager.getDefaultSharedPreferences(context);
+		
+		Boolean autoSwitch = preferences.getBoolean(Configuration.PREF_AUTO_SWITCH_ENABLED, 
+				Configuration.PREF_AUTO_SWITCH_ENABLED_DEFAULT);
+		
+		if (!autoSwitch)
+			return;
+		
+		final String action = intent.getAction();
 		
 		if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION))
 		{
@@ -30,12 +41,14 @@ public class WifiReceiver extends BroadcastReceiver
 				Log.d(Configuration.TAG, "Succesfully connected to Wi-fi");
 				
 				disableProxy();
+				disableU2NL();
 			}
 			else
 			{
 				Log.d(Configuration.TAG, "Wi-fi connection was dropped or disconnected");
 				
 				enableProxy();
+				enableU2NL();
 			}
 			
 		}
@@ -46,6 +59,7 @@ public class WifiReceiver extends BroadcastReceiver
 				Log.d(Configuration.TAG, "Wi-fi was disabled");
 				
 				enableProxy();
+				enableU2NL();
 			}
 		}
 		else if (action.equals(WifiManager.SUPPLICANT_CONNECTION_CHANGE_ACTION))
@@ -55,6 +69,7 @@ public class WifiReceiver extends BroadcastReceiver
 				Log.d(Configuration.TAG, "Wi-fi connection was dropped or disconnected");
 				
 				enableProxy();
+				enableU2NL();
 			}
 		}
 	}
@@ -64,7 +79,19 @@ public class WifiReceiver extends BroadcastReceiver
 		Log.d(Configuration.TAG, "Enabling proxy");
 		
 		ContentResolver res = context.getContentResolver();
-		String hostname = Configuration.DEFAULT_PROXY + ':' + Configuration.DEFAULT_PROXY_PORT;
+		
+		Boolean useCustomProxy = preferences.getBoolean(Configuration.PREF_USE_CUSTOM_PROXY, false);
+		String customProxy = preferences.getString(Configuration.PREF_PROXY, Configuration.PREF_PROXY_DEFAULT);
+		String customProxyPort = preferences.getString(Configuration.PREF_PROXY_PORT, Configuration.PREF_PROXY_PORT_DEFAULT);
+		
+		String hostname;
+		
+		if (useCustomProxy)
+			hostname = customProxy + ':' + customProxyPort;
+		else
+			hostname = Configuration.DEFAULT_PROXY 
+						+ ':' + Configuration.DEFAULT_PROXY_PORT;
+		
 		
 		Settings.Secure.putString(res, Settings.Secure.HTTP_PROXY, hostname);
 		context.sendBroadcast(new Intent(Proxy.PROXY_CHANGE_ACTION));
@@ -79,6 +106,28 @@ public class WifiReceiver extends BroadcastReceiver
 		
 		Settings.Secure.putString(res, Settings.Secure.HTTP_PROXY, hostname);
 		context.sendBroadcast(new Intent(Proxy.PROXY_CHANGE_ACTION));
+	}
+	
+	private void enableU2NL()
+	{
+		Boolean useU2NL = preferences.getBoolean(Configuration.PREF_USE_U2NL, 
+				Configuration.PREF_USE_U2NL_DEFAULT);
+		
+		if (!useU2NL)
+			return;
+		
+		Log.d(Configuration.TAG, "Enabling U2NL");
+	}
+	
+	private void disableU2NL()
+	{
+		Boolean useU2NL = preferences.getBoolean(Configuration.PREF_USE_U2NL, 
+				Configuration.PREF_USE_U2NL_DEFAULT);
+		
+		if (!useU2NL)
+			return;
+		
+		Log.d(Configuration.TAG, "Enabling U2NL");
 	}
 
 }
